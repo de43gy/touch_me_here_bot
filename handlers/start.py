4233430bf6825bb6bee9e96b2a8f5e07e30bfbcc
Ko <de43gy@gmail.com>
2025-01-29 21:47:02 +0400

    Implement user registration and logging in the /start command with error handling

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,14 @@
 from aiogram import types
-from keyboard import main_menu
+from keyboards import main_menu
+import logging
 
-async def smd_start(message: types.Message):
-    await message.answer("Привет! Я помогу хочешь получить массаж или сделать его?", reply_markup=main_menu)
+logger = logging.getLogger(__name__)
+
+async def cmd_start(message: types.Message):
+    try:
+        from database import add_user
+        await add_user(message.from_user.id, message.from_user.username)
+        await message.answer("Привет! Я бот TouchHarmony, помогу тебе записаться на массаж или сделать его.", reply_markup=main_menu)
+        logger.info(f"Пользователь {message.from_user.username} (ID: {message.from_user.id}) запустил бота.")
+    except Exception as e:
+        logger.error(f"Ошибка в команде /start для пользователя {message.from_user.username} (ID: {message.from_user.id}) ")
